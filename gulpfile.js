5430a8d7b326fefd7f6fcdde30f7ab710548105b
Dave Flynn <analogue40@gmail.com>
2017-08-17 15:50:53 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp'),
+		sass = require ('gulp-sass'),
+		notify = require('gulp-notify'),
+		filter = require('gulp-filter'),
+		autoprefixer = require('gulp-autoprefixer'),
+		concat = require('gulp-concat'),
+		uglify = require('gulp-uglify');
+		
+var config = {
+	stylesPath: 'assets/styles',
+	jsPath: 'assets/scripts',
+	outputDir: 'public/dist'
+}
+
+
+
+gulp.task('icons', function() { 
+	return gulp.src('./node_modules/font-awesome/fonts/**.*') 
+		.pipe(gulp.dest(config.outputDir + '/fonts')); 
+});
+
+gulp.task('css', function() {
+	return gulp.src(config.stylesPath + '/main.sass')
+		.pipe(sass({
+				outputStyle: 'compressed',
+				includePaths: [
+					config.stylesPath,
+					'./node_modules/bootstrap/scss',
+					'./node_modules/font-awesome/scss'
+				]
+			}).on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(config.outputDir + '/css'));
+});
+
+
+gulp.task('jquery', function(){
+	return gulp.src('./node_modules/jquery/dist/jquery.min.js') 
+		.pipe(gulp.dest(config.outputDir + '/js')); 
+});
+
+gulp.task('bootstrap-js', function(){
+	return gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js') 
+		.pipe(gulp.dest(config.outputDir + '/js')); 
+});
+
+gulp.task('js', function() {
+	return gulp.src(config.jsPath+'/*')
+		.pipe(filter('**/*.js'))
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(config.outputDir + '/js'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch([config.stylesPath + '**/*.scss', config.stylesPath + '**/*.sass', config.stylesPath + '**/*.css'], ['css']);
+	gulp.watch([config.jsPath + '**/*.js'], ['js']);
+});
+
+gulp.task('default', ['icons', 'css', 'jquery', 'bootstrap-js', 'js']);
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Bootstrap 4 project boilerplate
+
+A boilerplate for Bootstrap 4, FontAwesome and jQuery using Yarn and Gulp
+
+
+## Installation
+
+1. Clone this repo.
+2. cd into directory and run `bower install && npm install`
+3. Run `gulp` to compile Sass and JS
+4. Run `gulp watch` to monitor your JS and SASS
+
+## To do
+
+1. Image optimization
